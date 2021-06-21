ad608b33831902a1d51c31fd52a673b5665285b4
kalkithesaviour <vishal.adhikari36@gmail.com>
2021-06-22 04:58:02 +0530

    final commit

--- snake.py
+++ snake.py
@@ -21,7 +21,6 @@ def create_random_color():
 class Snake:
 
     def __init__(self):
-        super().__init__()
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
